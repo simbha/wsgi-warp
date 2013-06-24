9a6d2e9907b421cfccff1cf751d7c0bb103fda47
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2013-06-24 22:31:05 +1000

    Initial test version of dotcloud support.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name = 'warpdrive',
+      version = '0.9',
+      description = 'Launcher for SGI applications.',
+      author = 'Graham Dumpleton',
+      author_email = 'Graham.Dumpleton@gmail.com',
+      license = 'BSD',
+      url = 'https://github.com/GrahamDumpleton/wsgi-warp',
+      packages = ['warpdrive', 'warpdrive.dotcloud'],
+      scripts = ['warp-admin'],
+     )
---
+++ warpdrive/dotcloud/config.py
@@ -0,0 +1,51 @@
+import json
+
+APACHE_CONFIG = """
+ServerRoot /home/dotcloud/code/apache
+ErrorLog /home/dotcloud/logs/error_log
+PidFile /home/dotcloud/logs/apache.pid
+
+LoadModule authz_host_module /usr/lib/apache2/modules/mod_authz_host.so
+LoadModule mime_module /usr/lib/apache2/modules/mod_mime.so
+LoadModule rewrite_module /usr/lib/apache2/modules/mod_rewrite.so
+LoadModule wsgi_module /usr/lib/apache2/modules/mod_wsgi.so
+
+DefaultType text/plain
+TypesConfig /etc/mime.types
+
+MaxMemFree 64
+ThreadStackSize 262144
+
+ServerLimit 1
+ThreadLimit 15
+StartServers 1
+MaxClients 15
+MinSpareThreads 15
+MaxSpareThreads 15
+ThreadsPerChild 15
+MaxRequestsPerChild 0
+
+Listen %(PORT_WWW)s
+
+DocumentRoot /home/dotcloud/current/htdocs
+
+<Directory />
+    Options FollowSymLinks
+    AllowOverride None
+    Order deny,allow
+    Deny from all
+</Directory>
+
+<Directory /home/dotcloud/current/htdocs>
+    Options ExecCGI
+    AddHandler wsgi-script .wsgi
+    Order allow,deny
+    Allow from all
+</Directory>
+"""
+
+if __name__ == '__main__':
+    with open('/home/dotcloud/environment.json') as f:
+        environ = json.load(f)
+
+    print(APACHE_CONFIG % environ)
