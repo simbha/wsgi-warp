24f06e38e8319182a1d9b4c3036a14bd7ee32975
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2013-06-25 15:54:03 +1000

    Typo in description.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name = 'warpdrive',
       version = '0.9',
-      description = 'Launcher for SGI applications.',
+      description = 'Launcher for WSGI applications.',
       author = 'Graham Dumpleton',
       author_email = 'Graham.Dumpleton@gmail.com',
       license = 'BSD',
